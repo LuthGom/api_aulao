b865072990a48f5a74761606e0ba2436e83c94d7
LuthGom <lucianomendes23@yahoo.com>
2023-06-03 14:40:51 -0300

    feat: corrigindo campos de cadastro do db

--- src/Schemas/usuario.schemas.ts
+++ src/Schemas/usuario.schemas.ts
@@ -2,7 +2,7 @@ import { Schema } from "mongoose";
 import { IUsuario } from "../interfaces/usuario.interface";
 
 export const usuarioSchema = new Schema<IUsuario>({
-  nome: { type: String, require: true },
+  nome: { type: String, required: true },
   turma: {type: String, required: true},
   email: { type: String, required: true },
 });
